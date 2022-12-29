9b7ce303810957eecb6d3e2b5f946f10c731eb5e
Brandon Minjares <brandon.minjares@gmail.com>
2022-12-29 02:39:40 -0800

    Modified Emails into Table form

--- frontend/src/components/Emails.jsx
+++ frontend/src/components/Emails.jsx
@@ -1,5 +1,7 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import SharedContext from './SharedContext';
+import {TableContainer, Table, TableHead, TableBody, TableRow,
+  TableCell, Paper} from '@mui/material';
 
 const fetchMail = (mailbox, setMail) => {
   const item = localStorage.getItem('user');
@@ -36,26 +38,37 @@ const fetchMail = (mailbox, setMail) => {
  * @return {void}
  */
 export default function Emails() {
-  const {mailbox} = useContext(SharedContext);
-  const [mail, setMail] = useState([]);
+  const {mailbox, mail, setMail} = useContext(SharedContext);
   React.useEffect(() => {
     fetchMail(mailbox, setMail);
     // nothing in array, it will only run once
-  }, [mailbox]);
+  }, [mailbox, setMail]);
 
   return (
-    <div>
-      {mailbox}
-      {Array.isArray(mail) ?
-        mail.map((row) => (
-          <div key={row.mailid}>
-            <h2>{row.mail.subject}</h2>
-            <p>{row.mail.from}</p>
-            <p>{row.mail.content}</p>
-            <hr/>
-          </div>
-        )):
-        <p>No new mail!</p>}
-    </div>
-  );
+    <TableContainer component={Paper}>
+      <Table sx={{minWidth: 650}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">From</TableCell>
+            <TableCell align="right">Subject</TableCell>
+            <TableCell align="right">Content</TableCell>
+            <TableCell align="right">Sent</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {mail.map((row) => (
+            <TableRow
+              key={row.mailid}
+              sx={{'&:last-child td, &:last-child th': {border: 0}}}
+            >
+              <TableCell align="right">{row.mail.from}</TableCell>
+              <TableCell align="right">{row.mail.subject}</TableCell>
+              <TableCell align="right">{row.mail.content}</TableCell>
+              <TableCell align="right">{row.mail.sent}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+);
 }
